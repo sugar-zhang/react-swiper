3726d1fda586392222f79afa6f9920fbc222b045
sugar-zhang <xuejunzh_2527@qq.com>
2019-03-06 11:34:55 +0800

    demo

---
+++ build/precache-manifest.49089a70380e2bb70a21fd6f27210e6b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9eb600ee07a27cdad64f",
+    "url": "./static/js/runtime~main.9eb600ee.js"
+  },
+  {
+    "revision": "b38d799b81feed344bd4",
+    "url": "./static/js/main.b38d799b.chunk.js"
+  },
+  {
+    "revision": "5ec21d738fba382f2658",
+    "url": "./static/js/2.5ec21d73.chunk.js"
+  },
+  {
+    "revision": "b38d799b81feed344bd4",
+    "url": "./static/css/main.42179a9a.chunk.css"
+  },
+  {
+    "revision": "216b601723aacae696821ae7c04fb1bf",
+    "url": "./index.html"
+  }
+];
